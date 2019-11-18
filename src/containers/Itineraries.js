48c9368db4682b1f37863675f3760cc5336d89a6
avi <abhi.uk96@gmail.com>
2019-11-18 22:49:30 +0000

    import flights data and loop though

--- src/containers/Itineraries.js
+++ src/containers/Itineraries.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Aux from '../hoc/Aux';
 import Itinerery from '../components/Itinerery/Itinerery';
-
+import flights from '../data/flights.json';
+//console.log(flights);
 
 class Itineraries extends Component {
     render() {
-        return <Aux>
 
-            <Itinerery />
-            </Aux>;
+      const iternary = flights.itineraries.map((FlightDetails, index)=> {
+console.log(index);
+        return <Itinerery 
+                  key={index}
+                  id={index}
+                  agent={FlightDetails.agent}
+                  price={FlightDetails.price}
+                  agent_rating={FlightDetails.agent_rating}
+                  legs={FlightDetails.legs}
+                  />
+      });
+
+        return(
+          <Aux>
+            <div>{iternary}</div>
+          </Aux>
+        );
     }
+    
 }
 
 export default Itineraries;
